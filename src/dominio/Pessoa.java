ee4b9c1692a5a52821daf00dfcfa80d263190e7c
Adriano Rocha <adriano_rocha15@hotmail.com>
2019-06-09 18:36:40 -0300

    Adicionando objetos no banco de dados

--- src/dominio/Pessoa.java
+++ src/dominio/Pessoa.java
@@ -2,6 +2,11 @@ package dominio;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 
 /**
  * 
@@ -10,10 +15,14 @@ import java.io.Serializable;
  * 
  * @category classe responsável por representar a entidade Pessoa no sistema
  */
+@Entity
 public class Pessoa implements Serializable {
 	
 	
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer Id;
 	private String nome;
 	private String email;
