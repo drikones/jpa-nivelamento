791cec2ffa1d697eb6d5a100917794810fb5d5e4
Adriano Rocha <adriano_rocha15@hotmail.com>
2019-06-09 18:40:33 -0300

    Buscando uma pessoa no banco de dados

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -10,24 +10,13 @@ public class Programa {
 
 	public static void main(String[] args) {
 		
-		Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
-		Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquim@gmail.com");
-		Pessoa p3 = new Pessoa(null, "Ana Maria", "ana@gmail.com");
 		
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = emf.createEntityManager();
 		
-		em.getTransaction().begin();
-		
-		em.persist(p1);
-		em.persist(p2);
-		em.persist(p3);
-		
-		em.getTransaction().commit();
-		
-		System.out.println(p1);
-		System.out.println(p2);
-		System.out.println(p3);
+	    Pessoa p = em.find(Pessoa.class, 3);
+	    
+	    System.out.println(p);
 	}
 	
 }
